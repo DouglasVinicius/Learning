a0c34a3e32e3807140cc912425e3d83c918db083
DouglasVinicius <dododulli@gmail.com>
2021-09-14 15:48:16 -0300

    Code organization

--- Jogos/adivinhacao.py
+++ Jogos/adivinhacao.py
@@ -36,14 +36,31 @@ class Adivinhacao:
 
         return numero_repetido
 
+    def informa_jogador(self):
+        self.rodada += 1
+        self.restam_tentativas = (self.rodada < self.quantidade_de_tentativas_iniciais)
+        print("---------------------------------------------------------------")
+        print(f"Tentativa {self.rodada} de {self.quantidade_de_tentativas_iniciais}: ")
+
+    def verifica_acerto(self, numero_usuario):
+        acertou = (self.numero_adivinhar == int(numero_usuario))
+        maior = (self.numero_adivinhar < int(numero_usuario))
+        if(acertou):
+            self.rodada -= 1
+            pontuacao_final = float(((self.quantidade_de_tentativas_iniciais-self.rodada)*100)/self.quantidade_de_tentativas_iniciais)
+            str_pontuacao = "Sua pontuação final é: {:5.2f} de 100.00".format(pontuacao_final)
+            print("\nParabéns, você acertou o número!", str_pontuacao, sep="\n")
+            return 1
+        elif(self.restam_tentativas):
+            if(maior):
+                print("\nQue pena, você errou!", "\nDica: o número que você digitou é maior que o número secreto!!!", end="\n\n")
+            else:
+                print("\nQue pena, você errou!", "\nDica: o número que você digitou é menor que o número secreto!!!", end="\n\n")
+
     def main_loop(self):
         while(self.restam_tentativas):
             try:
-                self.rodada += 1
-                self.restam_tentativas = (self.rodada < self.quantidade_de_tentativas_iniciais)
-                print("---------------------------------------------------------------")
-                print(f"Tentativa {self.rodada} de {self.quantidade_de_tentativas_iniciais}: ")
-
+                self.informa_jogador()
                 numero_usuario = input("\nDigite seu número de 1 a 100, ou 'q' para sair: ")
                 sair = (numero_usuario == 'q')
                 
@@ -61,21 +78,11 @@ class Adivinhacao:
                 else:
                     if(sair):
                         print("O número secreto era o {}.".format(self.numero_adivinhar), "\nObrigado por jogar, mais sorte da próxima vez!")
-                        return
+                        return 1
                     else:
-                        acertou = (self.numero_adivinhar == int(numero_usuario))
-                        maior = (self.numero_adivinhar < int(numero_usuario))
-                        if(acertou):
-                            self.rodada -= 1
-                            pontuacao_final = float(((self.quantidade_de_tentativas_iniciais-self.rodada)*100)/self.quantidade_de_tentativas_iniciais)
-                            str_pontuacao = "Sua pontuação final é: {:5.2f} de 100.00".format(pontuacao_final)
-                            print("\nParabéns, você acertou o número!", str_pontuacao, sep="\n")
+                        if(self.verifica_acerto(numero_usuario)):
                             return 1
-                        elif(self.restam_tentativas):
-                            if(maior):
-                                print("\nQue pena, você errou!", "\nDica: o número que você digitou é maior que o número secreto!!!", end="\n\n")
-                            else:
-                                print("\nQue pena, você errou!", "\nDica: o número que você digitou é menor que o número secreto!!!", end="\n\n")
+
                     self.numeros_anteriores.append(numero_usuario)
             except:
                 self.rodada -= 1
