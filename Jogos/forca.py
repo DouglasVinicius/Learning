f5d1f8d688cb15088b6ccd00d06a9645f647a3b0
DouglasVinicius <dododulli@gmail.com>
2021-09-14 01:57:39 -0300

    Polishing and repair the game

--- Jogos/forca.py
+++ Jogos/forca.py
@@ -4,28 +4,42 @@ def jogar():
     print("***********************************************", end="\n\n")
 
     palavra_adivinhar = "palavra".lower()
-    palavra_na_tela = []
-    for i in range(0, len(palavra_adivinhar)):
-        palavra_na_tela.append('_')
+    palavra_na_tela = ["_" for letra in palavra_adivinhar]
 
     enforcou = False
     acertou = False
     palavra_descoberta = False
-    rodada = 0
+    erros = 0
+    quantidade_de_tentativas = 0
     letras_anteriores = []
+
+    while(True):
+        try:
+            print("Antes de iniciarmos, qual dificuldade você deseja para o jogo?")
+            print("(1) Muito fácil  (2) Fácil  (3) Médio  (4) Difícil  (5) Impossível")
+            index_dificuldade = int(input("Digite sua escolha: "))
+            dificuldades = (12, 9, 6, 4, 2)
+
+            if(index_dificuldade >= 1 and index_dificuldade <= 5):
+                quantidade_de_tentativas = dificuldades[index_dificuldade-1]
+            else:
+                print("Escolha incorreta, por favor digite uma opção válida!")
+                continue
+            break
+        except:
+            print("Escolha incorreta, por favor digite uma opção válida!")
 
     while((not enforcou) and (not palavra_descoberta)):
         try:
             letra_repetida = False
-            rodada += 1
             letras_faltando = palavra_na_tela.count('_')
 
             print("Palavra secreta até o momento: ")
             for i in palavra_na_tela:
                 print(i, end=' ')
-            print("\nVocê acertou {} de {} letras da palavra secreta!".format(len(palavra_na_tela)-letras_faltando, len(palavra_na_tela)))
-            letra_usuario = input("\n\nDigite sua letra de A a Z (não há distinção entre letras minúsculas e maiúsculas), ou '0' para sair: ").lower().replace(" ", "")
+            print("\nVocê acertou {} de {} letras da palavra secreta e errou {} de {} vezes!".format(len(palavra_na_tela)-letras_faltando, len(palavra_na_tela), erros, quantidade_de_tentativas))
 
+            letra_usuario = input("\n\nDigite sua letra de A a Z (não há distinção entre letras minúsculas e maiúsculas), ou '0' para sair: ").lower().replace(" ", "")
             try:
                 sair = (int(letra_usuario) == 0)
             except:
@@ -33,7 +47,6 @@ def jogar():
 
             if(not sair):
                 if(ord(letra_usuario) < 97 or ord(letra_usuario) > 122):
-                    rodada -= 1
                     print("Valor inválido, por favor digite uma letra de A a Z, ou '0'!", end="\n\n")
                     continue
 
@@ -45,7 +58,6 @@ def jogar():
             print("]")
 
             if(letra_repetida):
-                rodada -= 1
                 print("\nEssa letra já foi escolhida, por favor tente escolher novas letras!", "Lembre-se que letras maiúsculas e minúsculas são interpretadas da mesma forma!", sep="\n", end="\n\n")
             else:
                 if(sair):
@@ -58,20 +70,15 @@ def jogar():
 
                 if(acertou):
                     print("\nParabéns, a letra escolhida está contida na palavra!")
-                    for letra in palavra_na_tela:
-                        if(letra != '_'):
-                            palavra_descoberta = True
-                        else:
-                            palavra_descoberta = False
-                            break
+                    palavra_descoberta = "_" not in palavra_na_tela
                 else:
+                    erros += 1
                     print("Que pena, a letra escolhida não está contida na palavra secreta!", "Tente denovo", sep="\n")
 
-
+                enforcou = erros == quantidade_de_tentativas
                 acertou = False
                 letras_anteriores.append(letra_usuario)
         except:
-            rodada -= 1
             print("Valor inválido, por favor digite uma letra de A a Z, ou '0'!", end="\n\n")
 
     if(enforcou):
