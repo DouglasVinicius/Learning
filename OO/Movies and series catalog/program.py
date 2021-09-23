a3ef6a8e827c23a991f0ee57350a4cc74e4ab94d
DouglasVinicius <dododulli@gmail.com>
2021-09-23 03:20:06 -0300

    Mother class of Programs

---
+++ OO/Movies and series catalog/program.py
@@ -0,0 +1,29 @@
+class Program:
+    def __init__(self, name, release_year, genre):
+        self._name = name
+        self._release_year = release_year
+        self._genre = [x.title() for x in genre]
+        self._likes = 0
+
+    @property
+    def name(self):
+        return self._name.title()
+
+    @property
+    def release_year(self):
+        return self._release_year
+
+    @property
+    def duration_time(self):
+        return self._duration_time
+
+    @property
+    def genre(self):
+        return self._genre
+
+    @genre.setter
+    def genre(self, genre):
+        self._genre = [x.title() for x in genre]
+
+    def give_like(self):
+        self._likes += 1
