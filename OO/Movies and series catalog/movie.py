6b6e0106b3e3d695ae3cfa43e906611e53af48a3
DouglasVinicius <dododulli@gmail.com>
2021-09-24 01:13:39 -0300

    Finalized movie class

--- OO/Movies and series catalog/movie.py
+++ OO/Movies and series catalog/movie.py
@@ -8,3 +8,6 @@ class Movie(Program):
     @property
     def duration_time(self):
         return self.__duration_time
+
+    def print_prog(self):
+        print(f"Name: {self._name}, Realease Year: {self._release_year}, Genre: {self._genre}, Duration: {self.__duration_time}min, Current likes: {self._likes}")
